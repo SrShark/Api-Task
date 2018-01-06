478b5100270749935eea9ea09375fc8588bd6e4d
Guido Cavallo <SrShark@users.noreply.github.com>
2018-01-06 18:49:35 -0300

    servidor basico con express y configurado babel para ES6

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict'
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app. listen(port, () => {
+  console.log(`server on localhost:${port}`)
+})
